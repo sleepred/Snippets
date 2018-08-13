330ca4e339b32bae28a8c57a604658220c07781c
Ray S. Lee <sleepred@gmail.com>
2018-08-14 05:49:45 +0900

    Update CoinChange.java

--- java/CoinChange.java
+++ java/CoinChange.java
@@ -1,3 +1,27 @@
+/**
+주어진 금액 N과 S = {S1, S2, ..., Sm}인 동전 리스트에 대해 
+해당 동전 리스트로 주어진 금액 N을 만들 수 있는 조합의 개수를 구하시오. (동전의 순서는 고려하지 않음)
+
+예1)
+N = 4, S = {1, 2, 3}
+정답 : 4개 {1,1,1,1},{1,1,2},{2,2},{1,3}
+
+예2)
+N = 10, S = {2, 5, 3, 6}
+정답 : 5개 {2,2,2,2,2},{2,2,3,3},{2,2,6},{2,3,5},{5,5}
+
+public class CoinChange {
+	public static int countCoinChangeCase(int amount, int[] v) {
+		//TODO
+	}
+
+	public static void main(String[] args) {
+		int amount = 5;
+		int[] v = { 1, 2, 3 };
+		System.out.println(countCoinChangeCase(amount, v));
+	}
+}
+*/
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
